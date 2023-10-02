566323e54307fbc5262605ffbd8abb258f1e27d0
cirno <cirnoqvq@163.com>
2023-10-02 14:16:08 +0800

    update: env example types

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,10 @@
 /// <reference types="astro/client" />
 /// <reference types="vitest" />
+
+interface ImportMetaEnv {
+  readonly CESIUM_TOKEN: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
