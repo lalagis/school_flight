12b0f6c85b0885d32fb6466490bf7f99c7805bae
cirno <cirnoqvq@163.com>
2023-10-04 00:23:16 +0800

    update: camera flyto school

--- src/components/Viewer.tsx
+++ src/components/Viewer.tsx
@@ -1,16 +1,43 @@
 import 'cesium/Build/Cesium/Widgets/widgets.css'
 
-import { Cartesian3, Color } from 'cesium'
-import { Entity, PointGraphics, Viewer as CesiumViewer } from 'resium'
+import { Cartesian3, Color, Ion } from 'cesium'
+import { useEffect, useState } from 'react'
+import { CameraFlyTo, Entity, PointGraphics, Viewer as ResiumViewer } from 'resium'
+import { useSnapshot } from 'valtio'
+
+import { envStore } from '../stores/env'
 
 const position = Cartesian3.fromDegrees(113.936372, 22.535233, 100)
 
 export default function Viewer() {
+  const env = useSnapshot(envStore)
+
+  const [loaded, setLoaded] = useState(false)
+
+  useEffect(() => {
+    if (!loaded && env) {
+      Ion.defaultAccessToken = env.CESIUM_TOKEN
+      setLoaded(true)
+    }
+  }, [env, loaded])
+
+  if (!loaded) return null
+
   return (
-    <CesiumViewer full>
+    <ResiumViewer
+      full
+      homeButton={false}
+      sceneModePicker={false}
+      baseLayerPicker={false}
+      navigationHelpButton={false}
+      geocoder={false}
+      animation={false}
+      timeline={false}
+    >
       <Entity position={position} name="school" description="Hello, world.">
-        <PointGraphics pixelSize={20} color={Color.FUCHSIA} />
+        <PointGraphics pixelSize={15} color={Color.FUCHSIA} />
       </Entity>
-    </CesiumViewer>
+      <CameraFlyTo destination={Cartesian3.fromDegrees(113.936372, 22.535233, 5000)} />
+    </ResiumViewer>
   )
 }
--- src/stores/env.ts
+++ src/stores/env.ts
@@ -1,6 +1,6 @@
 import { proxy } from 'valtio'
 
-export const env = proxy({
+export const envStore = proxy({
   CESIUM_TOKEN:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2NzcxOWQzNS05YmU5LTQwZTMtOGQyZi0zMjY3ZDRkMTFiMjkiLCJpZCI6MTY5NTM4LCJpYXQiOjE2OTYyMTc1NTN9.Or956MhURrUL_JGyjLl9zzuIn2G2grsl80WoBZM6fDc',
 })
